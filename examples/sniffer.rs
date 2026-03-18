442d49d517490763e82bda3bec9b1d5a07689d51
Arnaud Taffanel <arnaud@bitcraze.io>
2026-03-18 11:26:15 +0100

    Add broadcast address on pipe 1 to sniffer example

--- examples/sniffer.rs
+++ examples/sniffer.rs
@@ -2,13 +2,14 @@ use crazyradio::{Channel, Crazyradio, Datarate};
 use std::time::Duration;
 
 fn main() -> Result<(), crazyradio::Error> {
-    // Open the second Crazyradio (index 1), assumes the first one is used by the client
-    let mut cr = Crazyradio::open_nth(0)?;
+    // Open Crazyradio
+    let mut cr = Crazyradio::open_first()?;
 
     // Configure radio parameters before entering sniffer mode
     cr.set_channel(Channel::from_number(80)?)?;
     cr.set_datarate(Datarate::Dr2M)?;
     cr.set_address(&[0xe7, 0xe7, 0xe7, 0xe7, 0xe7])?;
+    cr.set_sniffer_address(1, &[0xff, 0xe7, 0xe7, 0xe7, 0xe7])?;
 
     println!("Entering sniffer mode on channel 80, 2Mbps ...");
     cr.enter_sniffer_mode()?;
